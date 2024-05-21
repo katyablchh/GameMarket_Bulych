2420119f90cf69d1fc3824c891106fbebf5a6968
katyablchh <158029917+katyablchh@users.noreply.github.com>
2024-05-21 22:11:17 +0300

    d

--- market.js
+++ market.js
@@ -1,172 +1,182 @@
-// Перевірка підключеного файлу скриптів
+// //Перевірка підключеного файлу скриптів
 // console.log('Перевірка підключеного файлу скриптів market.js')
-
-let itemsArray = [
- {
-    firstName:"Ірина",
-    lastName:"Боровська-Карандюк",
-    age:20,
-    subject:"Українська мова, українська література, зарубіжна література",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/borovska-karandyuk-iryna-anatoliyivna-683x1024.jpg",
-    
- },
- {
-    firstName:"Василь",
-    lastName:"Бабій",
-    age:30,
-    subject:"Фізична культура",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/babiy-vasyl-dmytrovych-683x1024.jpg",
-
- },
- {
-    firstName:"Вікторія",
-    lastName:"Нелипович",
-    age:20,
-    subject:"Математика",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/nelypovych-viktoriya-vitaliyivna-683x1024.jpg",
-  
- },
- {
-    firstName:"Іван",
-    lastName:"Севрук",
-    age:30,
-    subject:"Захист України",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/sevruk-ivan-hryhorovych-683x1024.jpg",
- },
- {
-    firstName:"Анжела",
-    lastName:"Лознюк",
-    age:20,
-    subject:"Українська література",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/10/photo_2023-10-25_15-46-33-1-693x1024.jpg",
-  
- },
- {
-    firstName:"Наталія",
-    lastName:"Зинюк",
-    age:20,
-    subject:"Хімія",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2024/01/zynjuk-natalija-mykolayivna-683x1024.jpg",
-  
- },
- {
-    firstName:"Наталія",
-    lastName:"Венцель",
-    age:20,
-    subject:"Директор",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg",
-  
- },
- {
-    firstName:"Лариса",
-    lastName:"Забелло",
-    age:20,
-    subject:"Англійська мова",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/zabello-larysa-oleksandrivna-682x1024.jpg",
-  
- },
- {
-    firstName:"Ірина",
-    lastName:"Глібко",
-    age:20,
-    subject:"Англійська мова",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/01/hlibko-iryna-anatoliyevna.jpg",
-  
- },
- {
-    firstName:"Тарас",
-    lastName:"Савінков",
-    age:30,
-    subject:"Англійська мова",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/savinkov-taras-yevhenovych-683x1024.jpg",
-  
- },
- {
-    firstName:"Марія",
-    lastName:"Медведєва",
-    age:20,
-    subject:"Географія",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/medvedyeva-mariya-vasylivna-683x1024.jpg",
-  
- },
- {
-    firstName:"Віталій",
-    lastName:"Шатківський",
-    age:30,
-    subject:"Інформатика",
-    photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
-  
- },
-
- ]
-
-// Отримання елементу з ідентифікатором items
-let itemsDiv = document.getElementById("items");
-
-
-//Перевірка існування знайденого блоку 
-if(itemsDiv) {
-
-    //Вивід знайденого елементу 
-    console.log(itemsDiv)
-    // Вивід значення поля знайденого елементу
-    // console.log('Поле classList: ', itemsDiv.classList)
-    // console.log('Поле id: ', itemsDiv.id)
-    // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
-
-    // // Додавання тексту в блок
-    // itemsDiv.innerText='Перший Програмно додано текст'
-    // itemsDiv.innerText+='Другий Програмно додано текст'
-
-    // //Додавання відформатованого HTML коду в блок 
-    // itemsDiv.innerHTML = '<h1>Відформатований HTML код</h1>'
-    // itemsDiv.innerHTML = '<div class = "item"></div>'
-    // itemsDiv.innerHTML += '<div class = "item"></div>' 
-    // itemsDiv.innerHTML += '<div class = "item"></div>' 
-    // itemsDiv.innerHTML += '<div class = "item"></div>'
-    itemsArray.forEach((item,index) =>{
-       // console.log(item)
-       //Спеціальний апостроф - Англійська розкладка - біля кнопки 1~
-       //Виводимо на веб сторінку елемент масиву в блок з класом item
-       itemsDiv.innerHTML += 
-       `
-       <div class = "item">
-       <h2>Вчитель №${index +1 } з ${itemsArray.length}</h2>
-       <p>${item.lastName} ${item.firstName}</p>
-       <p><img src="${item.photo}" alt="${item.lastName} ${item.firstName}" class=""</p>
-       <p>Вік: ${item.age}</p>
-       </div>
-       `
-    })
-
-    // for (let i =0; i<=100; i++){
-    //     itemsDiv.innerHTML += '<div class = "item"></div>'}  
-
-} else {
-
-    //Вивід повідомлення про не знайдений блок 
-    console.log("Блок товарів не знайдено")
-}
-
+// // Отримання елементу з ідентифікатором items
+// let itemsDiv =document.getElementById("items"); 
 //Визначення масиву товарів
-// let itemsArray = ['','','','']
-// console.log(itemsArray)
+let itemsArray = [
+    {
+        name: "Мотокоса 43",
+        
+        price: "5499 ",
+        discountPrice: "4497 ",
+        couponPrice: "4395",
+        
+        photo: "https://hotline.ua/img/tx/319/3193450945.jpg"
+    },
+    {
+        name: "Електричний тример 110",
+        price: "4497 ",
+        discountPrice: "3498",
+        couponPrice: "3396",
+        photo: "https://static.dnipro-m.ua/origin/description/6566/design/m-110.jpg"
+    },
+    {
+        name: "Електрична газонокосарка 32",
+         price: "4497",
+        discountPrice: "3297",
+        couponPrice: "3195",
+        photo: "https://static.dnipro-m.ua/origin/description/6567/design/electric-mower-32.jpg"
+    },
+    {
+        name: "Акумуляторний оприскувач 12 N",
+        price: "2190 ",
+        discountPrice: "1698",
+        couponPrice: "1596",
+        photo: "https://hotline.ua/img/tx/449/4492289865.jpg"
+    }  ,
+   
+ //{
+  //  firstName:"Ірина",
+  //  lastName:"Боровська-Карандюк",
+  //  age:20,
+  //  subject:"Українська мова, українська література, зарубіжна література",
+  //  photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/borovska-karandyuk-iryna-anatoliyivna-683x1024.jpg",
+    
+ //},
+// {
+ //   firstName:"Василь",
+  //  lastName:"Бабій",
+ //   age:30,
+ //   subject:"Фізична культура",
+ //   photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/babiy-vasyl-dmytrovych-683x1024.jpg",
 
-
-
-console.log(itemsArray)
-
-//Сортування масиву
-// itemsArray = itemsArray.sort()
-
-
-//Виведення в консоль номерів та значень елементів масиву
-// for(let i = 0; i < itemsArray.length; i++){
-//    console.log(i +'-й елемент:', itemsArray[i])
-// }
-
-//Виведення елементів відсортовоного масиву
-// itemsArray.sort().forEach((item, index) =>{
-//     console.log(index + '-й елемент:',item)
-// })
+ //},
+ //{
+ //   firstName:"Вікторія",
+ //   lastName:"Нелипович",
+   // age:20,
+  //  subject:"Математика",
+    //photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/nelypovych-viktoriya-vitaliyivna-683x1024.jpg",
+  
+ //},
+ //{
+   // firstName:"Іван",
+    //lastName:"Севрук",
+    //age:30,
+ //   subject:"Захист України",
+   // photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/sevruk-ivan-hryhorovych-683x1024.jpg",
+ //},
+ //{
+   // firstName:"Анжела",
+   // lastName:"Лознюк",
+   // age:20,
+   // subject:"Українська література",
+   // photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/10/photo_2023-10-25_15-46-33-1-693x1024.jpg",
+  
+ //},
+ //{
+   // firstName:"Наталія",
+   // lastName:"Зинюк",
+   // age:20,
+   // subject:"Хімія",
+   // photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2024/01/zynjuk-natalija-mykolayivna-683x1024.jpg",
+  
+// },
+ //{
+  //  firstName:"Наталія",
+   // lastName:"Венцель",
+   // age:20,
+   // subject:"Директор",
+   // photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg",
+  
+ //},
+ //{
+  //  firstName:"Лариса",
+  //  lastName:"Забелло",
+   // age:20,
+  //  subject:"Англійська мова",
+   // photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/zabello-larysa-oleksandrivna-682x1024.jpg",
+  
+// },
+ //{
+  //  firstName:"Ірина",
+  //  lastName:"Глібко",
+  //  age:20,
+   // subject:"Англійська мова",
+  //  photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/01/hlibko-iryna-anatoliyevna.jpg",
+  
+ //},
+ //{
+  //  firstName:"Тарас",
+   // lastName:"Савінков",
+  //  age:30,
+  //  subject:"Англійська мова",
+  //  photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/savinkov-taras-yevhenovych-683x1024.jpg",
+  
+ //},
+// {
+ //   firstName:"Марія",
+  //  lastName:"Медведєва",
+   // age:20,
+   // subject:"Географія",
+   // photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/medvedyeva-mariya-vasylivna-683x1024.jpg",
+  
+ //},
+ //{
+  //  firstName:"Віталій",
+   // lastName:"Шатківський",
+   // age:30,
+   // subject:"Інформатика",
+   // photo:"https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
+  
+// },
+//     'Газоноосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+//     'Газоноосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+//     'Газоноосарка 43',
+//     'Електричний тример 110',
+//     'Електрична газонокосарка 32',
+//     'Акумуляторний оприскувач 12 N',
+    
+]
+// Отримання елементу з індифікатором items
+let itemsDiv = document.getElementById("items");
+// Перевірка існування знайденого блоку 
+if (itemsDiv) { 
+    // Створення блоків по кількості елементів масиву
+    itemsArray.forEach((item, index) => {
+        // Виводимо на веб-сторінку елемент масиву в блок з класом item
+        itemsDiv.innerHTML += `
+    
+        <div class="item">
+        <div class="bober">
+            <div class="item-title">${item.name}</div>
+        </div>
+        <div class="item-image">
+        <img src="${item.photo}"  class="item-image">
+           
+            
+        </div>
+        <div class="parts-pay">
+            <div><img src="img-market/mono-lapka.png" alt="">6</div>
+            <div><img src="img-market/pb.png" alt="">8</div>
+        </div>
+        <div class="prise">
+            <div><span>${item.price}</span><sup>грн</sup></div>
+            <div><span>${item.discountPrice}</span><sup>грн</sup></div>
+        </div>
+        <div class="prise bonus">
+            <div>ціна за купоном</div>
+            <div><span>${item.couponPrice}</span><sup>грн</sup></div>
+        </div>
+    </div>`;
+});
+} else {
+// Вивід повідомлення про не знайдений блок 
+console.log('Блок товарів не знайдено');
+}
